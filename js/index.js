932559c8256f545a1f7ec930969304afb0e3232e
Daulet <anahaev@outlook.com>
2020-05-25 22:53:07 +0300

    First stage work with menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
-console.log('log');
+// Menu
+console.log('Test: JS works!');
+const leftMenu = document.querySelector('.left-menu');
+const hamdurger = document.querySelector('.hamburger');
+
+
+// Open/Close menu
+hamdurger.addEventListener('click', () => {
+  
+  leftMenu.classList.toggle('openMenu');
+  hamdurger.classList.toggle('open');
+
+});
+
+document.addEventListener('click', event => {
+
+  const target = event.target;
+
+  if (!target.closest('.left-menu')) {
+    leftMenu.classList.remove('openMenu');
+    hamdurger.classList.remove('open');
+  }
+
+});
+
+leftMenu.addEventListener('click', event => {
+  
+  const target = event.target;
+  const dropdown = target.closest('.dropdown');
+  
+  if (dropdown) {
+    dropdown.classList.toggle('active');
+    leftMenu.classList.add('openMenu');
+    hamdurger.classList.add('open');
+  }
+
+});
